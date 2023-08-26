b9317b536947c58db1076588533d5f76a20d1f2f
Erol Mahmutovic <erol.mahmutovic74@gmail.com>
2023-08-26 23:24:22 +0200

    basic crud

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
 
     path('', views.home, name="home"),
+
+    path('create-recipe/', views.createRecipe, name="create-recipe"),
+    path('update-recipe/<str:pk>/', views.updateRecipe, name="update-recipe"),
+    path('delete-recipe/<str:pk>/', views.deleteRecipe, name="delete-recipe"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,10 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Recipe
-# Create your views here.
+from .forms import RecipeForm
 
 
 def home(request):
     recipes = Recipe.objects.all()
     context = {'recipes': recipes}
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
+
+
+def createRecipe(request):
+    form = RecipeForm()
+    if request.method == 'POST':
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/recipe_creation.html', context)
+
+
+def updateRecipe(request, pk):
+    recipe = Recipe.objects.get(id=pk)
+    form = RecipeForm(instance=recipe)
+
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/recipe_creation.html', context)
+
+
+def deleteRecipe(request, pk):
+    recipe = Recipe.objects.get(id=pk)
+    if request.method == 'POST':
+        recipe.delete()
+        return redirect('home')
+
+    return render(request, 'base/delete.html', {'obj': recipe})
